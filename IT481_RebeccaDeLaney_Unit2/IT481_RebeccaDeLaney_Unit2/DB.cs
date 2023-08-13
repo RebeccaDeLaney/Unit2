13c1f5f7a265f520e4ed55abd429fb7352068c47
rebeccadelaney@student.purdueglobal.edu <141275640+RebeccaDeLaney@users.noreply.github.com>
2023-08-13 08:57:21 -0400

    Add files via upload

---
+++ IT481_RebeccaDeLaney_Unit2/IT481_RebeccaDeLaney_Unit2/DB.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT481_RebeccaDeLaney_Unit2
+{
+    class DB
+    {
+        string connectionString;
+        SqlConnection cnn;
+
+        public DB()
+        {
+            connectionString = "Server = LINDI\\SQLEXPRESS01; " +
+                              "Trusted_Connection = true; " +
+                              "Database = Northwind; " +
+                              "User Instance = false; " +
+                              "Connection timeout = 30";
+        }
+
+        public DB(string conn) 
+        {
+            connectionString = conn;
+        }
+
+        public string getCustomerCount()
+        {
+            Int32 count = 0;
+
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string countQuery = "SELECT COUNT(*) FROM dbo.Customers;";
+            SqlCommand cmd = new SqlCommand(countQuery, cnn);
+
+            try
+            {
+                count = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception ex) { 
+                Console.WriteLine(ex.Message);  
+            }
+            return count.ToString();
+        }
+
+        public string getCompanyNames()
+        {
+            string names = "";
+            SqlDataReader dataReader;
+
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string nameQuery = "SELECT CompanyName FROM dbo.Customers;";
+            SqlCommand cmd = new SqlCommand(nameQuery, cnn);
+            dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                try
+                {
+                    names = names + dataReader.GetValue(0) + "\n";
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return names;
+        }
+
+        public string getOrderCount()
+        {
+            Int32 count = 0;
+
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string countQuery = "SELECT COUNT(*) FROM dbo.Orders;";
+            SqlCommand cmd = new SqlCommand(countQuery, cnn);
+
+            try
+            {
+                count = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return count.ToString();
+        }
+
+        public string getOrders()
+        {
+            string orderIDs = "";
+            SqlDataReader dataReader;
+
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string nameQuery = "SELECT OrderID FROM dbo.Orders;";
+            SqlCommand cmd = new SqlCommand(nameQuery, cnn);
+            dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                try
+                {
+                    orderIDs = orderIDs + dataReader.GetValue(0) + "\n";
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return orderIDs;
+        }
+
+        public string getEmployeeCount()
+        {
+            Int32 count = 0;
+
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string countQuery = "SELECT COUNT(*) FROM dbo.Employees;";
+            SqlCommand cmd = new SqlCommand(countQuery, cnn);
+
+            try
+            {
+                count = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return count.ToString();
+        }
+
+        public string getEmployeeLastNames()
+        {
+            string orderIDs = "";
+            SqlDataReader dataReader;
+
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string nameQuery = "SELECT LastName FROM dbo.Employees;";
+            SqlCommand cmd = new SqlCommand(nameQuery, cnn);
+            dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                try
+                {
+                    orderIDs = orderIDs + dataReader.GetValue(0) + "\n";
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return orderIDs;
+        }
+    }
+}
---
+++ IT481_RebeccaDeLaney_Unit2/IT481_RebeccaDeLaney_Unit2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT481_RebeccaDeLaney_Unit2
+{
+    public partial class Form1 : Form
+    {
+        DB database;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            database = new DB("Server = LINDI\\SQLEXPRESS01; " +
+                              "Trusted_Connection = true; " +
+                              "Database = Northwind; " +
+                              "User Instance = false; " +
+                              "Connection timeout = 30");
+            MessageBox.Show("Connection information sent.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string count = database.getCustomerCount();
+            MessageBox.Show(count, "Customer Count");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string names = database.getCompanyNames();
+            MessageBox.Show(names, "Company Names");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string count = database.getOrderCount();
+            MessageBox.Show(count, "Order Count");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string orderid = database.getOrders();
+            MessageBox.Show(orderid, "OrderID");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string count = database.getEmployeeCount();
+            MessageBox.Show(count, "Employee Count");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string names = database.getEmployeeLastNames();
+            MessageBox.Show(names, "Employee Last Names");
+        }
+
+    }
+}
